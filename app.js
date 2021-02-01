b21902ae99296573130367e838549b457d145181
asad462 <mirasaduddinadil@gmail.com>
2021-02-01 15:51:53 +0530

    test

--- app.js
+++ app.js
@@ -8,27 +8,25 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function constructURL(text){
+function constructURL(text) {
     return serverURL + "?" + "text=" + text
 }
-function errorHandler(error){
-    console.log("Error occured",error)
+function errorHandler(error) {
+    console.log("Error occured",error);
     alert("Something wrong with server! Try again next time.")
 
 }
  
 
 function clickEventHandler() {
-    //outputDiv.innerText = "Transalated : "+ txtInput.nodeValue; 
-    //Used if we want to write the value somewhere
     var inputText = txtInput.value;
 
     fetch(constructURL(inputText))
-    .then[response => response.json]
-    .then[json => {
+    .then(response => response.json)  
+    .then(json => { 
         var translatedText = json.contents.translated;
         outputDiv.innerText = translatedText;
-    }]
+      })
     .catch(errorHandler)   //No need to pass argument to this callback function. 
 }
 
